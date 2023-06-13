ff31423b87e4be30a3679ce7f5c66df9765d9923
emceekoenig <129642888+emceekoenig@users.noreply.github.com>
2023-06-13 16:20:35 -0500

    Fixed bug in JS

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,27 @@
 function updateTime() {
   // Los Angeles
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   // Sydney
   let sydneyElement = document.querySelector("#sydney");
-  let sydneyDateElement = sydneyElement.querySelector(".date");
-  let sydneyTime = moment().tz("Australia/Sydney");
-  let sydneyTimeElement = sydneyElement.querySelector(".time");
-  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do, YYYY");
-  sydneyTimeElement.innerHTML = sydneyTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  if (sydneyElement) {
+    let sydneyDateElement = sydneyElement.querySelector(".date");
+    let sydneyTime = moment().tz("Australia/Sydney");
+    let sydneyTimeElement = sydneyElement.querySelector(".time");
+    sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do, YYYY");
+    sydneyTimeElement.innerHTML = sydneyTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
